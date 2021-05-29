82800b26c94f264e1d25207a076f48431ac79d02
Sebastian Godirio <sebastianlgodirio@gmail.com>
2021-05-29 13:53:17 -0300

    navbar agregado con react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Merceecom</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sebastian Godirio
-        </a>
-      </header>
+        <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {Navbar,Nav,NavDropdown} from "react-bootstrap";
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="danger"
+        variant="dark"
+      >
+        <Navbar.Brand href="#home">
+          Merceecom
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">
+              Productos
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              Nosotros
+            </Nav.Link>
+            <NavDropdown
+              title="Categorias"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                categoria1
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+              categoria2
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+              categoria3
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Todas las categorias
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">
+              Contacto
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
